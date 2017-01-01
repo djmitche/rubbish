8b59e775fd45d983cfd1adebee1764d7ee2508b2
Dustin J. Mitchell <dustin@mozilla.com>
2017-01-01 18:00:31 -0500

    factor out storage

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # cas - Content Addressible Storage
-
-# TO LEARN
-
- * why do I need `.to_string()` everywhere?
-
-# TODO
-
- * refactor hashing into hash.rs, so that the vec isn't public
---
+++ src/storage.rs
@@ -0,0 +1,68 @@
+use hash::Hash;
+use content::Content;
+use std::collections::HashMap;
+use rustc_serialize::{Decodable, Encodable};
+
+/// Type Storage provides a content-addressible storage pool.  The content
+/// inserted into the mechanism can be of any type implementing the `rustc_serialize`
+/// traits `Decodable` and `Encodable`.
+#[derive(Debug)]
+pub struct Storage<T: Encodable + Decodable> {
+    map: HashMap<Hash, Content<T>>,
+}
+
+impl <T: Encodable + Decodable> Storage<T> {
+    /// Create a new, empty storage pool.
+    pub fn new() -> Storage<T> {
+        Storage {
+            map: HashMap::new(),
+        }
+    }
+
+    /// Insert content into the storage pool, returning the Hash pointing to the content.
+    ///
+    /// Inserting the same content twice will result in the same Hash (and no additional
+    /// use of space).
+    pub fn store(&mut self, value: &T) -> Hash {
+        let (hash, encoded) = Content::encode(value);
+        self.map.insert(hash.clone(), encoded);
+        // TODO: detect collisions (requires copying encoded?)
+        return hash;
+    }
+
+    /// Retrieve content by hash.
+    pub fn retrieve(&self, hash: &Hash) -> Option<T> {
+        match self.map.get(hash) {
+            None => None,
+            Some(encoded) => Some(encoded.decode()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Storage;
+    use hash::Hash;
+
+    #[test]
+    fn put_get_strings() {
+        let mut storage = Storage::new();
+
+        let hash1 = storage.store(&"one".to_string());
+        let hash2 = storage.store(&"two".to_string());
+        let badhash = Hash::from_hex("000000");
+
+        assert_eq!(storage.retrieve(&hash1), Some("one".to_string()));
+        assert_eq!(storage.retrieve(&hash2), Some("two".to_string()));
+        assert_eq!(storage.retrieve(&badhash), None);
+    }
+
+    #[test]
+    fn put_twice() {
+        let mut storage = super::Storage::new();
+
+        let hash1 = storage.store(&"xyz".to_string());
+        let hash2 = storage.store(&"xyz".to_string());
+        assert_eq!(hash1, hash2);
+    }
+}
