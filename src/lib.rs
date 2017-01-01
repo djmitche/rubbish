3d005138bdb633cfed14046605bfef8b20490a0b
Dustin J. Mitchell <dustin@mozilla.com>
2017-01-01 14:02:24 -0500

    use byte vector for hashes

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,39 @@
 extern crate crypto;
 extern crate serde;
 extern crate bincode;
+extern crate rustc_serialize;
 
 use std::collections::HashMap;
+use std::fmt;
 use serde::{Serialize, Deserialize};
 use bincode::{SizeLimit};
 use bincode::serde::{serialize, deserialize};
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use rustc_serialize::hex::ToHex;
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct Hash(Vec<u8>);
+
+impl Hash {
+    pub fn to_hex(&self) -> String {
+        self.0[..].to_hex()
+    }
+}
+
+impl fmt::Debug for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0[..].to_hex())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Content(Vec<u8>);
 
 #[derive(Debug)]
 pub struct Storage {
-    map: HashMap<String, Vec<u8>>,
+    map: HashMap<Hash, Content>,
 }
-
-// TODO: use byte vector for hashes, too
 
 impl Storage {
     pub fn new() -> Storage {
@@ -23,14 +42,14 @@ impl Storage {
         }
     }
 
-    pub fn store<T: Serialize + Deserialize>(&mut self, content: &T) -> String {
+    pub fn store<T: Serialize + Deserialize>(&mut self, content: &T) -> Hash {
         let (hash, encoded) = hash_content(content);
-        self.map.insert(hash.to_string(), encoded);
+        self.map.insert(hash.clone(), encoded);
         // TODO: detect collisions (requires copying encoded?)
         return hash;
     }
 
-    pub fn retrieve<T: Serialize + Deserialize>(&self, hash: &String) -> Option<T> {
+    pub fn retrieve<T: Serialize + Deserialize>(&self, hash: &Hash) -> Option<T> {
         match self.map.get(hash) {
             None => None,
             Some(encoded) => Some(decode_content(encoded)),
@@ -38,16 +57,19 @@ impl Storage {
     }
 }
 
-fn hash_content<T: Serialize + Deserialize>(content: &T) -> (String, Vec<u8>) {
-    let encoded: Vec<u8> = serialize(content, SizeLimit::Infinite).unwrap();
+fn hash_content<T: Serialize + Deserialize>(content: &T) -> (Hash, Content) {
+    let encoded: Content = Content(serialize(content, SizeLimit::Infinite).unwrap());
 
     let mut sha = Sha256::new();
-    sha.input(&encoded[..]);
-    return (sha.result_str(), encoded);
+    sha.input(&encoded.0[..]);
+
+    let mut hash = Hash(vec![0; sha.output_bytes()]);
+    sha.result(&mut hash.0);
+    return (hash, encoded);
 }
 
-fn decode_content<T: Serialize + Deserialize>(encoded: &Vec<u8>) -> T {
-    deserialize(encoded).unwrap()
+fn decode_content<T: Serialize + Deserialize>(encoded: &Content) -> T {
+    deserialize(&encoded.0).unwrap()
 }
 
 #[cfg(test)]
@@ -58,10 +80,11 @@ mod tests {
 
         let hash1 = storage.store(&"one".to_string());
         let hash2 = storage.store(&"two".to_string());
+        let badhash = super::Hash(vec![0, 32]);
 
         assert_eq!(storage.retrieve::<String>(&hash1), Some("one".to_string()));
         assert_eq!(storage.retrieve::<String>(&hash2), Some("two".to_string()));
-        assert_eq!(storage.retrieve::<String>(&"xxx".to_string()), None);
+        assert_eq!(storage.retrieve::<String>(&badhash), None);
     }
 
     #[test]
@@ -87,12 +110,13 @@ mod tests {
     #[test]
     fn hash_content_of_string() {
         let (hash, encoded) = super::hash_content(&"abcd".to_string());
-        assert_eq!(hash, "9481cd49061765e353c25758440d21223df63044352cfde1775e0debc2116841");
-        assert_eq!(encoded, vec![0u8, 0, 0, 0, 0, 0, 0, 4, 97, 98, 99, 100]);
+        println!("{:?}", hash);
+        assert_eq!(hash.to_hex(), "9481cd49061765e353c25758440d21223df63044352cfde1775e0debc2116841");
+        assert_eq!(encoded, super::Content(vec![0u8, 0, 0, 0, 0, 0, 0, 4, 97, 98, 99, 100]));
     }
 
     #[test]
     fn decode_content_abcd() {
-        assert_eq!(super::decode_content::<String>(&vec![0u8, 0, 0, 0, 0, 0, 0, 4, 97, 98, 99, 100]), "abcd".to_string());
+        assert_eq!(super::decode_content::<String>(&super::Content(vec![0u8, 0, 0, 0, 0, 0, 0, 4, 97, 98, 99, 100])), "abcd".to_string());
     }
 }
