9a156cb434294e155cd2dbaf450225aff9b192af
Dustin J. Mitchell <dustin@mozilla.com>
2020-03-30 23:25:41 +0000

    Project 2: embrace the async

--- raft/src/net/local.rs
+++ raft/src/net/local.rs
@@ -1,51 +1,51 @@
-use crate::net::{NodeId, RaftNetworkNode};
+use crate::net::{Message, NodeId, RaftNetworkNode};
+use async_trait::async_trait;
 use failure::Fallible;
-use std::sync::mpsc;
-use std::thread;
+use tokio::sync::mpsc;
 
 /// Run a local network with the given number of nodes, calling the node_fn to
 /// represent each node.
-pub fn run_network(num_nodes: NodeId, node_fn: fn(LocalNode)) {
+pub fn make_network(num_nodes: NodeId) -> Vec<LocalNode> {
     let mut txs = vec![];
     let mut rxs = vec![];
 
     for _ in 0..num_nodes {
-        let (tx, rx) = mpsc::channel();
+        let (tx, rx) = mpsc::channel(100);
         txs.push(tx);
         rxs.push(Some(rx));
     }
 
-    let mut threads = vec![];
+    let mut nodes = vec![];
     for i in 0..num_nodes {
         let node = LocalNode {
             node_id: i,
             incoming: rxs[i].take().unwrap(),
             network: txs.clone(),
         };
-        threads.push(thread::spawn(move || node_fn(node)));
+        nodes.push(node);
     }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
+    nodes
 }
 
 /// A node in the local network, implementing RaftNetworkNode.
 pub struct LocalNode {
     pub node_id: NodeId,
-    incoming: mpsc::Receiver<(NodeId, Vec<u8>)>,
-    network: Vec<mpsc::Sender<(NodeId, Vec<u8>)>>,
+    incoming: mpsc::Receiver<(NodeId, Message)>,
+    network: Vec<mpsc::Sender<(NodeId, Message)>>,
 }
 
+#[async_trait]
 impl RaftNetworkNode for LocalNode {
-    fn send(&mut self, dest: NodeId, msg: Vec<u8>) -> Fallible<()> {
-        self.network[dest].send((self.node_id, msg))?;
+    async fn send(&mut self, dest: NodeId, msg: Message) -> Fallible<()> {
+        self.network[dest].send((self.node_id, msg)).await?;
         Ok(())
     }
 
-    fn recv(&mut self) -> Fallible<(NodeId, Vec<u8>)> {
-        let (src, msg) = self.incoming.recv()?;
-        Ok((src, msg))
+    async fn recv(&mut self) -> Fallible<(NodeId, Message)> {
+        if let Some((src, msg)) = self.incoming.recv().await {
+            return Ok((src, msg));
+        }
+        todo!()
     }
 }
 
@@ -53,17 +53,25 @@ impl RaftNetworkNode for LocalNode {
 mod test {
     use super::*;
 
-    #[test]
-    fn test_two_node_network() -> Fallible<()> {
-        let net = run_network(2, |mut node| {
-            if (node.node_id == 0) {
-                node.send(1, vec![1, 2, 3]);
-                node.send(1, vec![2, 3, 4]);
-            } else {
-                assert_eq!(node.recv().unwrap(), (0, vec![1, 2, 3]));
-                assert_eq!(node.recv().unwrap(), (0, vec![2, 3, 4]));
-            }
+    #[tokio::test]
+    async fn test_two_node_network() -> Fallible<()> {
+        let mut net = make_network(2);
+
+        let tasks = net.drain(..).map(move |mut node| {
+            tokio::spawn(async move {
+                if node.node_id == 0 {
+                    node.send(1, vec![1, 2, 3]).await.unwrap();
+                    node.send(1, vec![2, 3, 4]).await.unwrap();
+                } else {
+                    assert_eq!(node.recv().await.unwrap(), (0, vec![1, 2, 3]));
+                    assert_eq!(node.recv().await.unwrap(), (0, vec![2, 3, 4]));
+                }
+            })
         });
+
+        for task in tasks {
+            task.await?;
+        }
 
         Ok(())
     }
--- raft/src/net/mod.rs
+++ raft/src/net/mod.rs
@@ -1,13 +1,18 @@
+use async_trait::async_trait;
 use failure::Fallible;
 
 pub mod local;
+//pub mod tcp;
 
 pub type NodeId = usize;
 
+pub type Message = Vec<u8>;
+
+#[async_trait]
 pub trait RaftNetworkNode {
     /// Make a best effort to send the given message to the given node
-    fn send(&mut self, dest: NodeId, msg: Vec<u8>) -> Fallible<()>;
+    async fn send(&mut self, dest: NodeId, msg: Message) -> Fallible<()>;
 
     /// Receive the next message from any other node
-    fn recv(&mut self) -> Fallible<(NodeId, Vec<u8>)>;
+    async fn recv(&mut self) -> Fallible<(NodeId, Message)>;
 }
