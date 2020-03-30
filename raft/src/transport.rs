874edc50e7e33199b06c958242b70c758dcaa0c0
Dustin J. Mitchell <dustin@mozilla.com>
2020-03-30 15:38:39 +0000

    Project 1: recv_message

--- raft/src/transport.rs
+++ raft/src/transport.rs
@@ -2,9 +2,9 @@
 //!
 //! Framing is a 32-bit unigned length in network byte order, followed by the message content.
 
-use crate::util::writeall;
+use crate::util::{readall, writeall};
 use byteorder::{ByteOrder, NetworkEndian};
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpStream;
 
 pub fn send_message(sock: &mut TcpStream, msg: &[u8]) -> std::io::Result<()> {
     let mut lenbuf = [0u8; 4];
@@ -15,10 +15,21 @@ pub fn send_message(sock: &mut TcpStream, msg: &[u8]) -> std::io::Result<()> {
     writeall(sock, msg)
 }
 
+pub fn recv_message(sock: &mut TcpStream) -> std::io::Result<Vec<u8>> {
+    let mut lenbuf = [0u8; 4];
+    readall(sock, &mut lenbuf)?;
+
+    let len = NetworkEndian::read_u32(&lenbuf) as usize;
+
+    let mut msg = vec![0u8; len];
+    readall(sock, &mut msg)?;
+    Ok(msg)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::util::readall;
+    use std::net::TcpListener;
     use std::thread;
 
     fn threaded_test(server_fn: fn(&mut TcpStream) -> (), client_fn: fn(&mut TcpStream) -> ()) {
@@ -41,7 +52,7 @@ mod test {
     }
 
     #[test]
-    fn send_short_message() {
+    fn test_send_message() {
         threaded_test(
             |sock| {
                 send_message(sock, b"Hello").unwrap();
@@ -54,6 +65,21 @@ mod test {
                 let mut buf = [0; 5];
                 readall(sock, &mut buf).unwrap();
                 assert_eq!(&buf[..], b"Hello");
+            },
+        )
+    }
+
+    #[test]
+    fn test_recv_message() {
+        threaded_test(
+            |sock| {
+                // 4 bytes of length, 4 bytes of message
+                let buf = [0u8, 0, 0, 4, 100, 101, 102, 103];
+                writeall(sock, &buf).unwrap();
+            },
+            |sock| {
+                let msg = recv_message(sock).unwrap();
+                assert_eq!(&msg[..], &[100, 101, 102, 103]);
             },
         )
     }
