88c2b05224979f75e12a6cee58461f590510f075
Dustin J. Mitchell <dustin@mozilla.com>
2020-04-20 01:52:33 +0000

    upgrade dependency

--- src/util/test.rs
+++ src/util/test.rs
@@ -6,5 +6,5 @@ static LOGGER_INITIALIZED: Once = Once::new();
 /// Initialize `env_logger` on the first call, and do nothing thereafter.  This is useful in tests
 /// where the order of the tests is not defined, but each requires `env_logger`.
 pub fn init_env_logger() {
-    LOGGER_INITIALIZED.call_once(|| env_logger::init().unwrap());
+    LOGGER_INITIALIZED.call_once(|| env_logger::init());
 }
